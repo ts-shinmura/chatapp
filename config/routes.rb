eb061165e360705af422af41166a3d4aba7326ed
t-hane <t-hane@respect-pal.jp>
2023-06-08 06:51:12 +0900

    初版

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root redirect('chats')
+
+  post 'acs' => 'application#consume'
+
   resources :chats do
     resources :messages
   end
