dcdf6f2b5d8ff4155d09e5138e23e8bc0a480431
t-hane <t-hane@respect-pal.jp>
2023-06-07 16:15:27 +0900

    初版

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server 'acc01.respect.work', user: 'rails', roles: %w[app db web]
 
 # role-based syntax
 # ==================
