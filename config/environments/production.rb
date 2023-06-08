fe032f1c630ab53973337194cffa8d406c547ce9
t-hane <t-hane@respect-pal.jp>
2023-06-08 09:55:38 +0900

    初版

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   config.action_cable.url = "wss://ai-chat.respect.work/cable"
-  config.action_cable.allowed_request_origins = [ "http://ai-chat.respect.work" ]
+  config.action_cable.allowed_request_origins = [ "https://ai-chat.respect.work" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
